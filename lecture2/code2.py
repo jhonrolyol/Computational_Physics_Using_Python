d498decf4e7c151471048c796d85ea0061144e75
jhonrolyol <rolyordonezleon@gmail.com>
2024-02-15 04:22:37 -0500

    A change in the lecture2 folder

---
+++ lecture2/code2.py
@@ -0,0 +1,17 @@
+# Computational Physics Lecture 2, Introduction to Python
+
+t = float(input("Enter temperature:"))
+if t > 100:
+    print("Water is in gas state: ")
+elif t > 0 and t < 100:
+    print("Water is in liquid state: ")
+else:
+    print("It is ice")
+
+
+
+
+
+
+
+
